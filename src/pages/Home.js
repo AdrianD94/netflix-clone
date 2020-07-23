e849e25d9943b703eb53b502110c6a97b41d85b3
Ady Drozman <ady.drozman2007@gmail.com>
2020-07-23 23:55:53 +0300

    jumbotron work in progress

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+  return (
+    <Jumbotron.Container>
+      <Jumbotron.Title>hi</Jumbotron.Title>
+      <Jumbotron.SubTitle>hi</Jumbotron.SubTitle>
+    </Jumbotron.Container>
+  );
+}
