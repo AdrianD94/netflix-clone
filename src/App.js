85465e88ac079df405ff9aa7a52edbf8278f6ca8
Ady Drozman <ady.drozman2007@gmail.com>
2020-07-26 13:18:14 +0300

    done accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 
 export function App() {
